0ea9292271c52f160dbbdd0540d87bbfc797794d
WestMecRyan <129911377+WestMecRyan@users.noreply.github.com>
2025-10-19 23:24:05 -0700

    added button animation with map

--- my-react-apps-2025/src/components/CalcButton.jsx
+++ my-react-apps-2025/src/components/CalcButton.jsx
@@ -1,8 +1,17 @@
 // CalcButton.jsx
 import "../css/calc-button.css";
 
-function CalcButton({ textValue, sendToOutput }) {
-    return <div className="calcButton" onClick={()=>sendToOutput(textValue)}>{textValue}</div>;
+function CalcButton({ textValue, sendToOutput, index }) {
+  const animationDelay = `${index * 0.4}s`;
+  return (
+    <div
+      className="calcButton"
+      onClick={() => sendToOutput(textValue)}
+      style={{ animationDelay }}
+    >
+      {textValue}
+    </div>
+  );
 }
 
 export default CalcButton;
